696177ef70a566f25548560f5f5bcec6382e6809
Brandon Sanders <sanders@civicplus.com>
2017-03-22 09:49:43 -0400

    paradine updates

--- process.php
+++ process.php
@@ -4,6 +4,16 @@
  * @author Brandon Sanders
 */
 
+$processType = isset($_POST['processType']) ? $_POST['processType'] : '';
+$account = base64_decode(json_decode($processType, true));
+$token = isset($account['token']) ? $account['token'] : '';
+$type = isset($account['type']) ? $account['type'] : '';
 
-
+if($token == '900b451!z4k65009' && $type) {
+    switch($type) {
+        case 'proposal' :
+            echo 'proposal type found';
+            break;
+    }
+}
 ?>
